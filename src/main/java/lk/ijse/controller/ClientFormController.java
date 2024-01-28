37df29a88173713782c503aa85dd803de569017c
nuharaomesh <omeshnuhara4@gmail.com>
2024-01-28 17:10:14 +0530

    set reply message to client forms

--- src/main/java/lk/ijse/controller/ClientFormController.java
+++ src/main/java/lk/ijse/controller/ClientFormController.java
@@ -7,8 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import lk.ijse.service.ClientSide;
 
 import java.io.*;
+import java.net.Socket;
 
 public class ClientFormController {
 
@@ -16,12 +18,24 @@ public class ClientFormController {
     private VBox mainContainer;
     @FXML
     private TextField txtMsg;
+    ClientSide ob;
 
     public void initialize() throws IOException {
 
+        ob = new ClientSide();
+        new Thread(ob).start();
+
+        ob.valueProperty().addListener(((observableValue, oldValue, newValue) -> {
+
+            Label label = new Label(newValue);
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER_LEFT);
+            hBox.getChildren().add(label);
+            mainContainer.getChildren().add(hBox);
+        }));
     }
     @FXML
     void btnSendOnAction(ActionEvent event) throws IOException {
-
+        ob.sendClient(txtMsg.getText());
     }
 }

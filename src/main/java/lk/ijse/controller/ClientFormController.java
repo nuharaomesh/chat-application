6807b0d5ed76be08c3005dfc123e9776142e6fd9
nuharaomesh <omeshnuhara4@gmail.com>
2024-01-28 16:41:23 +0530

    update client form

--- src/main/java/lk/ijse/controller/ClientFormController.java
+++ src/main/java/lk/ijse/controller/ClientFormController.java
@@ -1,5 +1,27 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.*;
+
 public class ClientFormController {
 
+    @FXML
+    private VBox mainContainer;
+    @FXML
+    private TextField txtMsg;
+
+    public void initialize() throws IOException {
+
+    }
+    @FXML
+    void btnSendOnAction(ActionEvent event) throws IOException {
+
+    }
 }
